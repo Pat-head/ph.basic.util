1ee2ba2bb1b27a3f620ac2ff636219764d1e29d8
xiong.xiaoyu <xiong.xiaoyu@synyi.com>
2023-03-07 11:00:00 +0800

    feat: add StringExtensions

---
+++ src/PatHead.Basic.Util/TypeExtensions/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PatHead.Basic.Util.TypeExtensions
+{
+    /// <summary>
+    /// StringExtensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        private static readonly Regex DigitRegex = new Regex("^\\d+$");
+
+        /// <summary>
+        /// IsDigit
+        /// </summary>
+        /// <param name="text">text</param>
+        /// <returns></returns>
+        public static bool IsDigit(this string text)
+        {
+            return !text.IsNullOrWhiteSpace() && DigitRegex.IsMatch(text);
+        }
+
+        /// <summary>
+        /// IsNullOrEmpty
+        /// </summary>
+        /// <param name="text">text</param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+
+        /// <summary>
+        /// IsNullOrEmpty
+        /// </summary>
+        /// <param name="text">text</param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+    }
+}
