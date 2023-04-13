50214b2e3045f6622956297546c481d4f096ad3f
xiong.xiaoyu <xiong.xiaoyu@synyi.com>
2023-04-13 14:08:57 +0800

    feat: add DClone

---
+++ src/PatHead.Basic.Util/TypeExtensions/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace PatHead.Basic.Util.TypeExtensions
+{
+    /// <summary>
+    /// ObjectExtensions
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Deep Clone
+        /// </summary>
+        /// <param name="source"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T DClone<T>(this T source)
+        {
+            var settings = new JsonSerializerSettings();
+            return source.ToJson(settings).ToObject<T>();
+        }
+    }
+}
